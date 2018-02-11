baace97f042ec0d5de241e2d9865a44c8256b261
luuufan <angelia.fan@gmail.com>
2018-02-10 16:28:14 -0800

    simple d3 chart practice

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+module.exports = {
+  context: __dirname,
+  entry: './lib/entry.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
